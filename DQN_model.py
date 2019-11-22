70576821c277f63405c49cd28224b29d0d961a02
Anish Muthali <anishmuthali@gmail.com>
2019-11-21 20:43:42 -0800

    Added gitignore + bad DQN model

--- DQN_model.py
+++ DQN_model.py
@@ -3,27 +3,28 @@ import gym
 import gym_donkeycar
 import numpy as np
 from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines.deepq.policies import MlpPolicy
-from stable_baselines import DQN
+from stable_baselines.ddpg.policies import CnnPolicy
+from stable_baselines import DDPG
 
 #SET UP ENVIRONMENT
-os.environ['DONKEY_SIM_PATH'] = f"./DonkeySimMac/donkey_sim.app/Contents/MacOS/donkey_sim"
+os.environ['DONKEY_SIM_PATH'] = f"../DonkeySimMac/donkey_sim.app/Contents/MacOS/donkey_sim"
 os.environ['DONKEY_SIM_PORT'] = str(9091)
-os.environ['DONKEY_SIM_HEADLESS'] = str(1) # "1" is headless
+os.environ['DONKEY_SIM_HEADLESS'] = str(0) # "1" is headless
 
 env = gym.make("donkey-warehouse-v0")
 #gym.make("donkey-generated-roads-v0")
 
 timesteps = 100000 # Set this to a reasonable number
-model_name = "dqn_model" # Change the model name to your preferences
+model_name = "ddpg_model" # Change the model name to your preferences
 training = True # Change this to test or use the model
 
 if training:
-    model = DQN(MlpPolicy, env, verbose=1)
+    vec_env = DummyVecEnv([lambda: env])
+    model = DDPG(CnnPolicy, vec_env, verbose=1)
     model.learn(total_timesteps=timesteps)
     model.save(model_name)
 else:
-    model = DQN.load(model_name)
+    model = DDPG.load(model_name)
     obv = env.reset()
     for t in range(10000):
         action, _states = model.predict(obv) # drive straight with small speed
