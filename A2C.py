3d345a6ba0a56b5e0c6cf2f102db26419a5c4791
anishmuthali <anishmuthali@gmail.com>
2019-12-01 13:53:31 -0800

    Fixed multiprocessing code and checkpointing

--- A2C.py
+++ A2C.py
@@ -6,22 +6,27 @@ from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines.common.policies import CnnPolicy, CnnLstmPolicy
 from stable_baselines.results_plotter import *
 from stable_baselines.bench import Monitor
-from stable_baselines import DDPG, A2C, PPO2
+from stable_baselines import A2C, PPO2
 
 best_reward = -np.inf
 num_steps = 0
+num_cpu = 6
 
 def callback(_locals, _globals):
     global best_reward, num_steps
-    if (num_steps + 1) % 500 == 0:
-        x, y = ts2xy(load_results("models/"), 'timesteps')
-        if len(x) > 0:
-            curr_reward = np.mean(y[-100:])
-            print(x[-1], 'timesteps')
-            if curr_reward > best_reward:
-                best_reward = curr_reward
-                print("Saving model")
-                _locals['self'].save("models/a2c.pkl")
+    if (num_steps + 1) % 100 == 0:
+        curr_reward = 0
+        for cpu in range(num_cpu):
+            x, y = ts2xy(load_results("models" + str(cpu) + "/"), 'timesteps')
+            if len(x) > 0:
+                curr_reward += np.mean(y[-100:])
+                print(x[-1], 'timesteps')
+        curr_reward = curr_reward/num_cpu
+        print("Current reward", curr_reward)
+        if curr_reward > best_reward:
+            best_reward = curr_reward
+            print("Saving model")
+            _locals['self'].save("models/a2c.pkl")
     num_steps += 1
     return True
 
@@ -29,26 +34,28 @@ def make(env_str, cpu):
     def make_helper():
         env = gym.make(env_str)
         env.seed(cpu)
-        env = Monitor(env, "models/", allow_early_resets=True)
+        env = Monitor(env, "models" + str(cpu) + "/", allow_early_resets=False)
         env.reset()
         return env
     return make_helper
 
 
 #SET UP ENVIRONMENT
-os.environ['DONKEY_SIM_PATH'] = f"../DonkeySimMac/donkey_sim.app/Contents/MacOS/donkey_sim"
+os.environ['DONKEY_SIM_PATH'] = "C:\\Users\\Anish Muthali\\Desktop\\SelfDriving\\DonkeySimWindows\\DonkeySim.exe"
 os.environ['DONKEY_SIM_PORT'] = str(9091)
 os.environ['DONKEY_SIM_HEADLESS'] = str(0) # "1" is headless
 os.environ['DONKEY_SIM_MULTI'] = str(1)
-os.makedirs("models/", exist_ok=True)
 
 timesteps = 100000 # Set this to a reasonable number
 model_name = "a2c_model" # Change the model name to your preferences
 training = True # Change this to test or use the model
-num_cpu = 1
 
-if training:
+for cpu in range(num_cpu):
+    os.makedirs("models" + str(cpu) + "/", exist_ok=True)
+
+if __name__ == "__main__" and training:
     #env = DummyVecEnv([lambda: env])
+    print("main")
     env = SubprocVecEnv([make("donkey-warehouse-v0", i) for i in range(num_cpu)])
     model = A2C(CnnLstmPolicy, env, verbose=1)
     print("Training")
